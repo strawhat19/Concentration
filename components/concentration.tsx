c0340e1b60d5cc86c7b451ea1208dbde35ed4d12
Rakib Ahmed <rakib987@gmail.com>
2023-06-14 00:17:39 -0400

    Auto Resetting Form

--- components/concentration.tsx
+++ components/concentration.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { useState } from 'react';
 import Box from '@mui/material/Box';
+import { dev } from '../pages/_app';
 import Step from '@mui/material/Step';
-import { dev } from "../../pages/_app";
 import Paper from '@mui/material/Paper';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -10,6 +9,7 @@ import Stepper from '@mui/material/Stepper';
 import StepLabel from '@mui/material/StepLabel';
 import Typography from '@mui/material/Typography';
 import StepContent from '@mui/material/StepContent';
+import { useState, useEffect, useRef } from 'react';
 import { TimeField } from '@mui/x-date-pickers/TimeField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -29,6 +29,7 @@ const muiTheme = createTheme({
 const steps = [
     {
         label: `Topic`,
+        description: `Select a Topic for the Game`,
     },
     {
         label: `Rules`,
@@ -42,20 +43,34 @@ const steps = [
 
 export default function Concentration(props) {
 
+    const loadedRef = useRef(false);
+    const [loaded, setLoaded] = useState(false);
     const [formFields, setFormFields] = useState({});
     const [activeStep, setActiveStep] = useState(0);
 
     const handleNext = (fields?) => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        let stepConnectors = document.querySelectorAll(`.MuiStepConnector-root`);
-        console.log(`Fields`, fields);
-        console.log(`activeStep`, activeStep);
-        console.log(`stepConnectors.length`, stepConnectors.length);
-        if (activeStep < stepConnectors.length) {
-            let thisConnector = stepConnectors[activeStep];
-            let connectorLineText = thisConnector.querySelector(`span`);
-            connectorLineText.innerHTML = Object.values(fields)[activeStep].toString();
+        setActiveStep((prevActiveStep) => {
+            return prevActiveStep + 1;
+        });
+
+        if (fields) {
+            let stepConnectors = document.querySelectorAll(`.MuiStepConnector-root`);
+            if (activeStep < stepConnectors.length) {
+                let thisConnector = stepConnectors[activeStep];
+                let connectorLineText = thisConnector?.querySelector(`span`);
+                connectorLineText.innerHTML = Object.values(fields)[activeStep]?.toString();
+            } else {
+            setTimeout(() => {
+                let lastCompleted = document.querySelectorAll(`.Mui-completed`)[document.querySelectorAll(`.Mui-completed`)?.length - 1]?.parentElement?.parentElement;
+                lastCompleted.insertAdjacentHTML(`afterend`, `<div class="MuiStepConnector-root MuiStepConnector-vertical Mui-completed css-1pe7n21-MuiStepConnector-root"><span class="customConnector MuiStepConnector-line MuiStepConnector-lineVertical css-8t49rw-MuiStepConnector-line">${Object.values(fields)[activeStep]?.toString()}</span></div>`);
+            }, 35);
+            }
         }
+
+        setTimeout(() => {
+            let nextInput: any = document.querySelectorAll(`.formField`)[document.querySelectorAll(`.formField`)?.length - 1].querySelector(`input`);
+            if (nextInput) nextInput?.focus();
+        }, 35);
     };
 
     const handleBack = () => {
@@ -64,6 +79,15 @@ export default function Concentration(props) {
 
     const handleReset = () => {
         setActiveStep(0);
+        let stepConnectors = document.querySelectorAll(`.MuiStepConnector-root`);
+        stepConnectors.forEach((step, index) => {
+            if (index == stepConnectors?.length - 1) {
+                step.remove();
+            } else {
+                step.firstElementChild.innerHTML = ``;
+            }
+        });
+        document.querySelector(`.formField`)?.querySelector(`input`)?.focus();
     };
 
     const ConcentrationGameFormSubmit = (ConcentrationGameFormSubmitEvent) => {
@@ -88,15 +112,43 @@ export default function Concentration(props) {
         let emptyVals = values.some(val => val == ``);
 
         if (emptyVals) {
-            console.log(`Empty`);
+            dev() && console.log(`Empty`);
+            return;
         } else {
+            dev() && console.log(`Game`, formFields);
             handleNext(formFields);
         }
     }
 
+    useEffect(() => {
+
+        if (activeStep == 0) {
+            document.querySelector(`.formField`)?.querySelector(`input`)?.focus()
+        } else if (activeStep === steps.length) {
+
+            const handleEnterKey = (event) => {
+                if (event.key === `Enter`) {
+                    handleReset();
+                }
+            };
+            
+            window.addEventListener(`keydown`, handleEnterKey);
+    
+            // Cleanup function to remove the event listener
+            return () => {
+                window.removeEventListener(`keydown`, handleEnterKey);
+            };
+        }
+
+        if (loadedRef.current) return;
+        loadedRef.current = true;
+        setLoaded(true);
+
+      }, [activeStep]);
+
     return <>
         <div id={`ConcentrationGame`} title={props.title} style={{width: `100%`, margin: `20px auto`}}>
-            <form className={`ConcentrationGameForm first`} id={`ConcentrationGameForm`} onSubmit={(ConcentrationGameFormSubmitEvent) => ConcentrationGameFormSubmit(ConcentrationGameFormSubmitEvent)}>
+            <form className={`ConcentrationGameForm`} id={`ConcentrationGameForm`} onSubmit={(ConcentrationGameFormSubmitEvent) => ConcentrationGameFormSubmit(ConcentrationGameFormSubmitEvent)}>
                 <ThemeProvider theme={muiTheme}>
                     <Box sx={{ maxWidth: 400 }}>
                         <Stepper activeStep={activeStep} orientation="vertical">
@@ -124,7 +176,7 @@ export default function Concentration(props) {
                                                 <TimeField style={{ marginTop: 15 }} format="HH:mm:ss" className={`formField timeLimit`} name={`timeLimit`} label="Time Limit" required />
                                             </LocalizationProvider>
                                         </>}
-                                        {activeStep != 0 && <Box sx={{ mb: 2 }}>
+                                        {/* {activeStep != 0 && <Box sx={{ mb: 2 }}>
                                             <div>
                                                 <Button
                                                     variant="contained"
@@ -141,7 +193,7 @@ export default function Concentration(props) {
                                                     Back
                                                 </Button>
                                             </div>
-                                        </Box>}
+                                        </Box>} */}
                                     </StepContent>
                                 </Step>
                             ))}
@@ -149,7 +201,7 @@ export default function Concentration(props) {
                         {activeStep === steps.length && (
                             <Paper square elevation={0} sx={{ p: 3 }}>
                                 <Typography>Game Set</Typography>
-                                <Button className={`MuiButton-containedPrimary containedPrimary`} onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                                <Button type={`submit`} className={`MuiButton-containedPrimary containedPrimary`} onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
                                     Start Game
                                 </Button>
                             </Paper>
